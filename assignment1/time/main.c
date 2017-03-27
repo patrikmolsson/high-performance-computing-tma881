28b959dff1a4fe79a5c8f9a8a50f1d596d903c30
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-03-27 15:43:04 +0200

    Making sure that we recompile between different opt flags

--- assignment1/time/main.c
+++ assignment1/time/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+long sumNFirst(long maxCap){
+  long sum = 0;
+  for (long i = 0; i <= maxCap; i++){
+    sum += i;
+  }
+  return sum;
+}
+
 int main() {
   long maxCap = 1000000000;
-  long sum;
+  long sum = sumNFirst(maxCap);
 
-  sum = maxCap*(maxCap+1)/2;
+  //long sum = maxCap*(maxCap+1)/2;
 
   printf("%li \n", sum);
   return 0;
