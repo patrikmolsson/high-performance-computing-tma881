5e1282c655b3ea60b7acbec29249c5a0ee62dfd0
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-03-27 14:26:54 +0200

    Added memory leaking application

---
+++ assignment1/valgrind/leak.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+void allocate_array(size_t n){
+  int *nelson_mandela;
+  nelson_mandela = (int *) malloc (sizeof(int) * n);
+  //free(nelson_mandela);
+}
+
+int main(){
+  size_t n = 1000;
+  allocate_array(n);
+}
+
