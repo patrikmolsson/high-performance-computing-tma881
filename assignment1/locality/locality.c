3e0e2370ea215e95aaa70ae0c23eb0fc64d7412e
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-03-29 13:18:47 +0200

    Changed to dynamic memory allocation for locality

--- assignment1/locality/locality.c
+++ assignment1/locality/locality.c
@@ -18,7 +18,7 @@ size_t rand_lim(size_t limit) {
 }
 
 //Changed from **matrix to matrix[][], is this ok?
-void row_sums(size_t nrs, size_t ncs,double sums[nrs], const double matrix[nrs][ncs]){
+void row_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs){
   for ( size_t ix=0; ix < nrs; ++ix ) {
     double sum = 0;
     for ( size_t jx=0; jx < ncs; ++jx )
@@ -27,7 +27,7 @@ void row_sums(size_t nrs, size_t ncs,double sums[nrs], const double matrix[nrs][
   }
 }
 
-void col_sums(size_t nrs, size_t ncs, double sums[nrs], const double matrix[nrs][ncs]){
+void col_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs){
   for ( size_t jx=0; jx < ncs; ++jx ) {
     double sum = 0;
     for ( size_t ix=0; ix < nrs; ++ix )
@@ -36,15 +36,15 @@ void col_sums(size_t nrs, size_t ncs, double sums[nrs], const double matrix[nrs]
   }
 }
 
-void col_sums2(size_t nrs, size_t ncs, double sums[nrs], const double matrix[nrs][ncs]){
+void col_sums2(double * sums, const double ** matrix, size_t nrs, size_t ncs){
   for ( size_t ix=0; ix < nrs; ++ix ){
     for ( size_t jx=0; jx < ncs; ++jx ) {
-        sums[jx] = sums[jx] + matrix[ix][jx];
+        sums[jx] += matrix[ix][jx];
     }
   }
 }
 
-int check_col_sums(size_t nrs, double sums[nrs], double sums2[nrs], double tol){
+int check_col_sums(double * sums, double * sums2, size_t nrs,  double tol){
   int same = 1;
   for ( size_t ix=0; ix < nrs; ++ix ){
     double diff = sums[ix] - sums2[ix];
@@ -56,7 +56,7 @@ int check_col_sums(size_t nrs, double sums[nrs], double sums2[nrs], double tol){
   return same;
 }
 
-void fill_matrix(size_t nrs, size_t ncs, double matrix[nrs][ncs], size_t limit){
+void fill_matrix(double ** matrix, size_t nrs, size_t ncs, size_t limit){
   for ( size_t ix=0; ix < nrs; ++ix ) {
     for ( size_t jx=0; jx < ncs; ++jx ){
       double tmp = rand_lim(limit);
@@ -68,34 +68,54 @@ void fill_matrix(size_t nrs, size_t ncs, double matrix[nrs][ncs], size_t limit){
 
 int main(){
   size_t nrs = 1000, ncs = 1000, limit = 100;
-  double matrix[nrs][ncs];
-  double sums[nrs];
-  double sums2[nrs];
+  double **matrix;
+  double *sums;
+  double *sums2;
   double tol = 0.001;
 
-  fill_matrix(nrs, ncs, matrix, limit);
+  // Allocate
+
+  matrix = malloc(nrs * sizeof *matrix);
+  sums = malloc(nrs * sizeof *sums);
+  sums2 = malloc(nrs * sizeof *sums);
+
+  for (size_t i=0; i < nrs; i++){
+    matrix[i] = malloc(ncs * sizeof *matrix[i]);
+  }
+
+  fill_matrix(matrix, nrs, ncs, limit);
 
   // Not sure if we should time here or use the makefile thingy
 
   clock_t begin = clock();
-  row_sums(nrs, ncs, sums, matrix);
+  row_sums(sums, (const double **)matrix, nrs, ncs);
   clock_t end = clock();
   double time_row_sums = (double)(end - begin) / CLOCKS_PER_SEC;
 
   begin = clock();
-  col_sums(nrs, ncs, sums, matrix);
+  col_sums(sums, (const double **)matrix, nrs, ncs);
   end = clock();
   double time_col_sums = (double)(end - begin) / CLOCKS_PER_SEC;
 
   begin = clock();
-  col_sums2(nrs, ncs, sums2, matrix);
+  col_sums2(sums2, (const double **)matrix, nrs, ncs);
   end = clock();
   double time_col_sums2 = (double)(end - begin) / CLOCKS_PER_SEC;
 
-  int same = check_col_sums(nrs, sums, sums2, tol);
+  int same = check_col_sums(sums, sums2, nrs, tol);
 
   printf("colsums time: %f colsums2 time: %f rowsums time: %f \n", time_col_sums, time_col_sums2, time_row_sums);
   printf("%s %d\n","Are colsum and colsum2 the same?", same);
+
+  //Deallocate
+
+  for (size_t i = 0; i < nrs; i++){
+    free(matrix[i]);
+  }
+
+  free(sums);
+  free(sums2);
+  free(matrix);
 
 	return 0;
 }
