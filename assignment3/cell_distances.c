42f4014f9cac9d81d6ea1ff5352b517bd25630b1
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-04-26 00:08:02 +0200

    Trying some more stuff on ass3

--- assignment3/cell_distances.c
+++ assignment3/cell_distances.c
@@ -6,13 +6,13 @@
 size_t num_threads;
 static const size_t max_pos = 3465;
 //static const double max_dist = 34.641016;//...
-static const float max_dist = 34.64f;
+static const double max_dist = 34.64f;
 
 void read_cells(){
   size_t lines = 0, n_coords = 3, fac = 100;
   char ch = 0;
-  FILE *fp = fopen("cells", "r");
-  float dist;
+  FILE *fp = fopen("cell_e5", "r");
+  double dist;
   while(!feof(fp))
   {
     ch = fgetc(fp);
@@ -21,25 +21,30 @@ void read_cells(){
       lines++;
     }
   }
-  float cell_array[lines][n_coords]; // double?
+  double cell_array[lines][n_coords]; // double?
   size_t *count_array;
-  count_array = malloc(max_pos*sizeof*count_array);
+  count_array = calloc(max_pos,sizeof*count_array);
+
   rewind(fp);
   for(size_t i = 0; i<lines; i++){
-    //fscanf(fp, "%lf %lf %lf", &cell_array[i][0], &cell_array[i][1], &cell_array[i][2]);
-    fscanf(fp, "%f %f %f", &cell_array[i][0], &cell_array[i][1], &cell_array[i][2]);
+    fscanf(fp, "%lf %lf %lf", &cell_array[i][0], &cell_array[i][1], &cell_array[i][2]);
   }
 
   for(size_t i = 0; i<lines; i++){
-    printf("%lf %lf %lf \n", cell_array[i][0], cell_array[i][1], cell_array[i][2]);
+    //printf("%lf %lf %lf \n", cell_array[i][0], cell_array[i][1], cell_array[i][2]);
   }
   fclose(fp);
+  #pragma omp parallel for collapse(2)
   for(size_t i = 0; i<lines; i++){
     for(size_t j = i + 1; j<lines; j++){
-      dist = sqrt(pow(cell_array[i][0]-cell_array[j][0],2)+
-                  pow(cell_array[i][1]-cell_array[j][1],2)+
-                  pow(cell_array[i][2]-cell_array[j][2],2));
+      dist = sqrt((cell_array[i][0]-cell_array[j][0])*
+                  (cell_array[i][0]-cell_array[j][0])+
+                  (cell_array[i][1]-cell_array[j][1])*
+                  (cell_array[i][1]-cell_array[j][1])+
+                  (cell_array[i][2]-cell_array[j][2])*
+                  (cell_array[i][2]-cell_array[j][2]));
       dist = round(dist*fac)/fac;
+      //printf("dist %lf\n",dist);
       count_array[(size_t)(dist/max_dist*max_pos)]++;
       //printf("dist %f ind %d\n",dist, (size_t)(dist/max_dist*max_pos));
     }
