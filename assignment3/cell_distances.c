07f6a4098190620460c218266f6bd9910cd630f6
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-04-26 17:57:55 +0200

    Fixed some formatting, added nested omp

--- assignment3/cell_distances.c
+++ assignment3/cell_distances.c
@@ -2,18 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
-size_t num_threads;
+size_t n_threads;
 static const size_t max_pos = 3465;
-//static const double max_dist = 34.641016;//...
 static const double max_dist = 34.64f;
+static const size_t fac = 100;
 
 void read_cells(){
-  size_t lines = 0, n_coords = 3, fac = 100;
-  char ch = 0;
-  //FILE *fp = fopen("cell_e4", "r");
-  FILE *fp = fopen("cell_e4", "r");
+  size_t lines = 0, n_coords = 3,i,j;
   float dist;
+  //char* filename = "cell_e5";
+  char* filename = "cell_e4";
+  //char* filename = "cells";
+  size_t *count_array;
+  float **cell_array;
+
+  FILE *fp = fopen(filename, "r");
+  if (!strcmp(filename,"cell_e5")){
+    lines = 100000;
+  } else if(!strcmp(filename,"cell_e4")){
+    lines = 10000;
+  } else if(!strcmp(filename,"cells")){
+    lines = 10;
+  }
+
+  /*char ch = 0;
   while(!feof(fp))
   {
     ch = fgetc(fp);
@@ -22,27 +36,27 @@ void read_cells(){
       lines++;
     }
   }
-  //double cell_array[lines][n_coords]; // double?
-  size_t *count_array;
-  float **cell_array;
+  rewind(fp);*/
+  count_array = calloc(max_pos,sizeof*count_array);
   cell_array = malloc(lines * sizeof *cell_array);
   for(size_t k = 0; k < lines; k++){
     cell_array[k] = malloc(n_coords *sizeof *cell_array[k]);
   }
-  count_array = calloc(max_pos,sizeof*count_array);
 
-  rewind(fp);
   for(size_t i = 0; i<lines; i++){
     fscanf(fp, "%f %f %f", &cell_array[i][0], &cell_array[i][1], &cell_array[i][2]);
   }
 
+  fclose(fp);
+
   //for(size_t i = 0; i<lines; i++){
   //  printf("%f %f %f \n", cell_array[i][0], cell_array[i][1], cell_array[i][2]);
   //}
-  fclose(fp);
-  size_t i,j;
-  #pragma omp parallel for private(i,j,dist) shared(cell_array,count_array,fac) num_threads(1)
+  #pragma omp parallel for private(i,j,dist) shared(cell_array,count_array) num_threads(n_threads)
+  //omp_set_nested(1);
+  //#pragma omp parallel for private(i) shared(cell_array,count_array) num_threads(n_threads)
   for(i = 0; i<lines; i++){
+    //#pragma omp parallel for private(j,dist) shared(cell_array,count_array) num_threads(2)
     for(j = i + 1; j<lines; j++){
       dist = sqrt((cell_array[i][0]-cell_array[j][0])*(cell_array[i][0]-cell_array[j][0])+
                   (cell_array[i][1]-cell_array[j][1])*(cell_array[i][1]-cell_array[j][1])+
@@ -51,16 +65,18 @@ void read_cells(){
       count_array[(size_t)(dist/max_dist*max_pos)]++;
     }
   }
+
   for(size_t i=0; i<max_pos;i++){
     if(count_array[i] != 0){
       printf("%1.2f %ld\n", max_dist*i/max_pos, count_array[i]);
     }
   }
-  free(count_array);
+
   for(size_t k = 0; k < lines; k++){
     free(cell_array[k]);
   }
   free(cell_array);
+  free(count_array);
 }
 
 int main(int argc, char *argv[]){
@@ -71,7 +87,7 @@ int main(int argc, char *argv[]){
       memset(arg, 0, sizeof(arg));
       memcpy(arg, &argv[i][2], strlen(argv[i]) - 2);
       if (argv[i][1] == 't') {
-        num_threads = (size_t) strtol(arg, NULL, 10);
+        n_threads = (size_t) strtol(arg, NULL, 10);
       }
 
     }
