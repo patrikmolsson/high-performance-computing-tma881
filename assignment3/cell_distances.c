09931587cbb7103d0e3f5955d780f81c16775e91
Per Nilsson Lundberg <pernlundberg@gmail.com>
2017-05-03 15:08:53 +0200

    Prealloc intrinsics

--- assignment3/cell_distances.c
+++ assignment3/cell_distances.c
@@ -62,13 +62,14 @@ void read_cells(){
   //for(size_t i = 0; i<lines*n_coords; i+=n_coords){
   //  printf("%f %f %f \n", cell_array[i], cell_array[i+1], cell_array[i+2]);
   //}
-  #pragma omp parallel for private(i,j,dist) shared(cell_array,count_array) num_threads(n_threads)
+  __m128 va,vb,diff_ab,mul_ab;
+  #pragma omp parallel for private(i,j,dist,va,vb,diff_ab,mul_ab) shared(cell_array,count_array) num_threads(n_threads)
   for(i = 0; i<lines*n_coords; i+=n_coords){
     for(j = i + n_coords; j<lines*n_coords; j+=n_coords){
-      __m128 va = _mm_load_ps(&cell_array[i]);
-      __m128 vb = _mm_load_ps(&cell_array[j]);
-      __m128 diff_ab = _mm_sub_ps(va, vb);
-      __m128 mul_ab = exp_ps(2*log_ps(diff_ab));
+      va = _mm_load_ps(&cell_array[i]);
+      vb = _mm_load_ps(&cell_array[j]);
+      diff_ab = _mm_sub_ps(va, vb);
+      mul_ab = exp_ps(2*log_ps(diff_ab));
       dist = hsum_ps_sse3(mul_ab);
       dist = round(sqrt(dist)*fac)/fac;
       count_array[(size_t)(dist/max_dist*max_pos)]++;
